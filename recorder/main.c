f12d19f6ca123dd1705cb48b3ff1a967875cdd7c
Kieran Elrod <kieranelrod@gmail.com>
2025-09-17 18:27:53 -0400

    copied in example depth capture

--- recorder/main.c
+++ recorder/main.c
@@ -1,53 +1,98 @@
-#pragma comment(lib, "k4a.lib")
-#include <k4a/k4a.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <k4a/k4a.h>
+#include <k4abt.h>
+
+#define VERIFY(result, error)                                                                            \
+    if(result != K4A_RESULT_SUCCEEDED)                                                                   \
+    {                                                                                                    \
+        printf("%s \n - (File: %s, Function: %s, Line: %d)\n", error, __FILE__, __FUNCTION__, __LINE__); \
+        exit(1);                                                                                         \
+    }                                                                                                    \
+
 int main()
 {
-    uint32_t count = k4a_device_get_installed_count();
-    if (count == 0)
-    {
-        printf("No k4a devices attached!\n");
-        return 1;
-    }
-
-    // Open the first plugged in Kinect device
     k4a_device_t device = NULL;
-    if (K4A_FAILED(k4a_device_open(K4A_DEVICE_DEFAULT, &device)))
+    VERIFY(k4a_device_open(0, &device), "Open K4A Device failed!");
+
+    // Start camera. Make sure depth camera is enabled.
+    k4a_device_configuration_t deviceConfig = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
+    deviceConfig.depth_mode = K4A_DEPTH_MODE_NFOV_UNBINNED;
+    deviceConfig.color_resolution = K4A_COLOR_RESOLUTION_OFF;
+    VERIFY(k4a_device_start_cameras(device, &deviceConfig), "Start K4A cameras failed!");
+
+    k4a_calibration_t sensor_calibration;
+    VERIFY(k4a_device_get_calibration(device, deviceConfig.depth_mode, deviceConfig.color_resolution, &sensor_calibration),
+        "Get depth camera calibration failed!");
+
+    k4abt_tracker_t tracker = NULL;
+    k4abt_tracker_configuration_t tracker_config = K4ABT_TRACKER_CONFIG_DEFAULT;
+    VERIFY(k4abt_tracker_create(&sensor_calibration, tracker_config, &tracker), "Body tracker initialization failed!");
+
+    int frame_count = 0;
+    do
     {
-        printf("Failed to open k4a device!\n");
-        return 1;
-    }
+        k4a_capture_t sensor_capture;
+        k4a_wait_result_t get_capture_result = k4a_device_get_capture(device, &sensor_capture, K4A_WAIT_INFINITE);
+        if (get_capture_result == K4A_WAIT_RESULT_SUCCEEDED)
+        {
+            frame_count++;
+            k4a_wait_result_t queue_capture_result = k4abt_tracker_enqueue_capture(tracker, sensor_capture, K4A_WAIT_INFINITE);
+            k4a_capture_release(sensor_capture); // Remember to release the sensor capture once you finish using it
+            if (queue_capture_result == K4A_WAIT_RESULT_TIMEOUT)
+            {
+                // It should never hit timeout when K4A_WAIT_INFINITE is set.
+                printf("Error! Add capture to tracker process queue timeout!\n");
+                break;
+            }
+            else if (queue_capture_result == K4A_WAIT_RESULT_FAILED)
+            {
+                printf("Error! Add capture to tracker process queue failed!\n");
+                break;
+            }
 
-    // Get the size of the serial number
-    size_t serial_size = 0;
-    k4a_device_get_serialnum(device, NULL, &serial_size);
+            k4abt_frame_t body_frame = NULL;
+            k4a_wait_result_t pop_frame_result = k4abt_tracker_pop_result(tracker, &body_frame, K4A_WAIT_INFINITE);
+            if (pop_frame_result == K4A_WAIT_RESULT_SUCCEEDED)
+            {
+                // Successfully popped the body tracking result. Start your processing
 
-    // Allocate memory for the serial, then acquire it
-    char *serial = (char*)(malloc(serial_size));
-    k4a_device_get_serialnum(device, serial, &serial_size);
-    printf("Opened device: %s\n", serial);
-    free(serial);
+                size_t num_bodies = k4abt_frame_get_num_bodies(body_frame);
+                printf("%zu bodies are detected!\n", num_bodies);
 
-    // Configure a stream of 4096x3072 BRGA color data at 30 frames per second -> Worry about depth later
-    k4a_device_configuration_t config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
-    config.camera_fps       = K4A_FRAMES_PER_SECOND_30;
-    config.color_format     = K4A_IMAGE_FORMAT_COLOR_BGRA32;
-    config.color_resolution = K4A_COLOR_RESOLUTION_3072P;
+                k4abt_frame_release(body_frame); // Remember to release the body frame once you finish using it
+            }
+            else if (pop_frame_result == K4A_WAIT_RESULT_TIMEOUT)
+            {
+                //  It should never hit timeout when K4A_WAIT_INFINITE is set.
+                printf("Error! Pop body frame result timeout!\n");
+                break;
+            }
+            else
+            {
+                printf("Pop body frame result failed!\n");
+                break;
+            }
+        }
+        else if (get_capture_result == K4A_WAIT_RESULT_TIMEOUT)
+        {
+            // It should never hit time out when K4A_WAIT_INFINITE is set.
+            printf("Error! Get depth frame time out!\n");
+            break;
+        }
+        else
+        {
+            printf("Get depth capture returned error: %d\n", get_capture_result);
+            break;
+        }
 
-    // Start the camera with the given configuration
-    if (K4A_FAILED(k4a_device_start_cameras(device, &config)))
-    {
-        printf("Failed to start cameras!\n");
-        k4a_device_close(device);
-        return 1;
-    }
+    } while (frame_count < 100);
 
-    // Camera capture and application specific code would go here
+    printf("Finished body tracking processing!\n");
 
-    // Shut down the camera when finished with application logic
+    k4abt_tracker_shutdown(tracker);
+    k4abt_tracker_destroy(tracker);
     k4a_device_stop_cameras(device);
     k4a_device_close(device);
 
